abc458d62f716bea59b138424e0bb0bc9ea4f4cd
Tomas Bonnedahl <tomas.bonnedahl@trioptima.com>
2017-02-23 18:29:19 +0100

    Finished file reader

--- qualify/qualify.py
+++ qualify/qualify.py
@@ -3,12 +3,14 @@ import heapq
 import numpy as np
 import time
 
+from qualify_utils import read_file
+
 EXAMPLE = "example.in"
 SMALL = "small.in"
 MEDIUM = "medium.in"
 MEDIUM_TEST = "medium-test.in"
 
-INPUT_FILE = MEDIUM_TEST
+INPUT_FILE = EXAMPLE
 
 
 class AlgoBase(object):
@@ -72,85 +74,83 @@ class AlgoBase(object):
             counter += 1
 
 
-class PizzaRunner(AlgoBase):
-    def __init__(self, ph, max_iter):
-        self.ph = ph
-        self.slice_hashes_visited = set()
-        self.initial_prio = 100000
-        self.prio_handler = PrioHandler(ph.max_cells_per_slice, init_prio=self.initial_prio)
-        self.finished_work = None
-        super(PizzaRunner, self).__init__(max_iter=max_iter)
-
-    def _get_init_prio_and_state(self):
-        initial_state = (ph.pizza, [])
-        return (self.initial_prio, initial_state)
-
-    def _get_moves(self):
-        all_coords = []
-        for coords in create_coords_medium(ph):
-            all_coords.append(coords)
-        return all_coords
-
-    def _valid(self, current_state, move):
-        current_pizza, current_slices = current_state
-        return validate_possible_slices_medium(current_pizza, move, self.ph)
-
-    def _not_visited(self, current_state, move):
-        current_pizza, current_slices = current_state
-        self.slices_coords = current_slices[:]
-        self.slices_coords.append(move)
-        slices_hashed = hash(tuple(sorted(self.slices_coords)))
-        visited = slices_hashed in self.slice_hashes_visited
-
-        if not visited:
-            self.slice_hashes_visited.add(slices_hashed)
-
-        return not visited
-
-    def _get_new_prio(self, current_prio, move):
-        r_start, c_start, r_finish, c_finish = move
-        slice_size = (r_finish - r_start) * (c_finish - c_finish)
-        return self.prio_handler.get_new_prio(current_prio, slice_size)
-
-    def _get_updated_state(self, current_state, move):
-        current_pizza, current_slices = current_state
-        r_start, c_start, r_finish, c_finish = move
-        updated_pizza = current_pizza.copy()
-        updated_pizza[r_start:r_finish, c_start:c_finish] = 0
-        return (updated_pizza, self.slices_coords)
-
-    def _first_solution(self):
-        return self.finished_work is None
-
-    def _better_solution(self, current_state):
-        current_pizza, _ = current_state
-        finished_pizza, finished_slices = self.finished_work
-        return np.count_nonzero(current_pizza) < np.count_nonzero(finished_pizza)
-
-    def _add_solution(self, current_state):
-        self.finished_work = current_state
-
-    def solution(self):
-        return self.finished_work
+# class PizzaRunner(AlgoBase):
+#     def __init__(self, ph, max_iter):
+#         self.ph = ph
+#         self.slice_hashes_visited = set()
+#         self.initial_prio = 100000
+#         self.prio_handler = PrioHandler(ph.max_cells_per_slice, init_prio=self.initial_prio)
+#         self.finished_work = None
+#         super(PizzaRunner, self).__init__(max_iter=max_iter)
+#
+#     def _get_init_prio_and_state(self):
+#         initial_state = (ph.pizza, [])
+#         return (self.initial_prio, initial_state)
+#
+#     def _get_moves(self):
+#         all_coords = []
+#         for coords in create_coords_medium(ph):
+#             all_coords.append(coords)
+#         return all_coords
+#
+#     def _valid(self, current_state, move):
+#         current_pizza, current_slices = current_state
+#         return validate_possible_slices_medium(current_pizza, move, self.ph)
+#
+#     def _not_visited(self, current_state, move):
+#         current_pizza, current_slices = current_state
+#         self.slices_coords = current_slices[:]
+#         self.slices_coords.append(move)
+#         slices_hashed = hash(tuple(sorted(self.slices_coords)))
+#         visited = slices_hashed in self.slice_hashes_visited
+#
+#         if not visited:
+#             self.slice_hashes_visited.add(slices_hashed)
+#
+#         return not visited
+#
+#     def _get_new_prio(self, current_prio, move):
+#         r_start, c_start, r_finish, c_finish = move
+#         slice_size = (r_finish - r_start) * (c_finish - c_finish)
+#         return self.prio_handler.get_new_prio(current_prio, slice_size)
+#
+#     def _get_updated_state(self, current_state, move):
+#         current_pizza, current_slices = current_state
+#         r_start, c_start, r_finish, c_finish = move
+#         updated_pizza = current_pizza.copy()
+#         updated_pizza[r_start:r_finish, c_start:c_finish] = 0
+#         return (updated_pizza, self.slices_coords)
+#
+#     def _first_solution(self):
+#         return self.finished_work is None
+#
+#     def _better_solution(self, current_state):
+#         current_pizza, _ = current_state
+#         finished_pizza, finished_slices = self.finished_work
+#         return np.count_nonzero(current_pizza) < np.count_nonzero(finished_pizza)
+#
+#     def _add_solution(self, current_state):
+#         self.finished_work = current_state
+#
+#     def solution(self):
+#         return self.finished_work
 
 
 start_time = time.time()
 
-big_ph = make_pizza_from_file(INPUT_FILE)
+big_ph = read_file(INPUT_FILE)
 # ph_list = partition_pizza(big_ph)
-big_ph.start_row = 0
-big_ph.start_col = 0
-ph_list = [big_ph]
+ph_list = []
 
 solutions = []
 
 for index, ph in enumerate(ph_list):
-    runner = PizzaRunner(ph, max_iter=50)
+    # runner = PizzaRunner(ph, max_iter=50)
     print('Running partition {}...').format(index+1)
-    runner.run()
-    solutions.append((ph, runner.solution()))
+    # runner.run()
+    # solutions.append((ph, runner.solution()))
 
-unpack_solutions_and_write_file(solutions)
+# unpack_solutions_and_write_file(solutions)
 
 print
 print 'Took {} second'.format(time.time() - start_time)
--- qualify/qualify_utils.py
+++ qualify/qualify_utils.py
@@ -4,9 +4,9 @@ import numpy as np
 def read_file(input_file_name):
     f_in = open(input_file_name, 'r')
     header = map(int, f_in.readline().rstrip().split())
-    a, b, c = map(int, header)
+    # a, b, c = header
 
     for row_index, line in enumerate(f_in):
-        for col_index, ingredient in enumerate(line.rstrip()):
+        for col_index, col_data in enumerate(line.rstrip()):
             pass
 
